4c2b9fc44370955f0147a6c74df12e9fa61db6d5
Steve Markham <steve.markham@gmail.com>
2019-01-17 21:00:17 -0500

    rename r_dot_px to r_px, to clarify units

--- get_r_M.m
+++ get_r_M.m
@@ -1,12 +1,10 @@
-function [r_dot_px, M_check] = get_r_M(r_dot_desired_px, threshold)
+function [r_px, M_check] = get_r_M(r_desired_px, threshold)
 
 if abs(threshold-0.35)<1e-3
     % I precomputed a bunch of values for threshold=0.35, and the answer turns
     % out to be linear, so we can vastly speed up the iterative search by
     % starting with a really good guess.
-    M_guess = round(polyval([3.2024 -1.4988], r_dot_desired_px));
-elseif threshold==0
-    M_guess = 0;
+    M_guess = round(polyval([3.2024 -1.4988], r_desired_px));
 else
     % Did some more precomputing, and for any given threshold, the value
     % is linear, and the slope as a function of threshold is roughly
@@ -16,28 +14,29 @@ else
     p_fit_M_check(1,1) = polyval(p_M_check,threshold);
     p_fit_M_check(1,2) = 0.5;
     
-    M_guess = round(polyval(p_fit_M_check, r_dot_desired_px));
+    M_guess = round(polyval(p_fit_M_check, r_desired_px));
 end
-%disp(sprintf('Guessed %d', M_guess));
+disp(sprintf('Guessed %d', M_guess));
 
-r_dot_px = r_dot_desired_px + M_guess;
+r_px = r_desired_px + M_guess;
 
-bitmap_row = [zeros(1,r_dot_px+1) ones(1,r_dot_px+1)];
-disk_filter = fspecial('disk', r_dot_px);
+bitmap_row = [zeros(1,r_px+1) ones(1,r_px+1)];
+disk_filter = fspecial('disk', r_px);
 filtered_row = imfilter(bitmap_row, disk_filter, 'replicate');
-r_actual = r_dot_px+2 - find(filtered_row>threshold,1);
-M_check =r_dot_px - r_actual;
+r_actual = r_px+2 - find(filtered_row>threshold,1);
+M_check = r_px - r_actual;
 
 while M_check ~= M_guess
     M_guess = M_check;
     disp(sprintf('Guessed %d', M_guess));
 
-    r_dot_px = r_dot_desired_px + M_guess;
+    r_px = r_desired_px + M_guess;
     
-    bitmap_row = [zeros(1,r_dot_px+1) ones(1,r_dot_px+1)];
-    disk_filter = fspecial('disk', r_dot_px);
+    bitmap_row = [zeros(1,r_px+1) ones(1,r_px+1)];
+    disk_filter = fspecial('disk', r_px);
     filtered_row = imfilter(bitmap_row, disk_filter, 'replicate');
-    r_actual = r_dot_px+2 - find(filtered_row>threshold,1);
-    M_check = r_dot_px - r_actual;
+    r_actual = r_px+2 - find(filtered_row>threshold,1);
+    M_check = r_px - r_actual;
     
 end
+
